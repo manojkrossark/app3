4adca8a40894e6bf7bdd89d46afe2dd7f25bdf82
manojkumar <69884509+manojkrossark@users.noreply.github.com>
2023-09-22 12:35:49 +0530

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
+const cors_1 = require("cors");
+const express_1 = require("express");
 const http_1 = require("http");
 const socket_io_1 = require("socket.io");
 // 加载 .env 环境变量
